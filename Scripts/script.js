9f6ab36405207c834dbcb2cf8fc95f4199306414
Globateur <45522552+Globateur@users.noreply.github.com>
2021-04-03 15:57:23 +0200

    VideoPlayer more opptimised
    + fin de unity.html
    Manque programme

--- Scripts/script.js
+++ Scripts/script.js
@@ -58,5 +58,30 @@ if (page == "Unity%20cours%20en%20ligne.html"){
 
 
   document.getElementById('vid1').height = (parentWidth * document.getElementById('vid2').width / 100) * 720 / 1280;
-  document.getElementById('vid2').height = (parentWidth * document.getElementById('vid2').width / 100) * 720 / 1280;
+  document.getElementById('vid2').height = (parentWidth * document.getElementById('vid2').width / 100) * 720 / 1280 - 4;
+
+  var video = document.getElementById('vid2');
+
+  function checkScroll() {
+      var fraction = 0.8; // Play when 80% of the player is visible.
+
+      var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
+              b = y + h, //bottom
+              visibleX, visibleY, visible;
+
+              visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+              visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+              visible = visibleX * visibleY / (w * h);
+
+              if (visible > fraction) {
+                  video.play();
+              } else {
+                  video.pause();
+              }
+
+  }
+
+  window.addEventListener('scroll', checkScroll, false);
+  window.addEventListener('resize', checkScroll, false);
 }
